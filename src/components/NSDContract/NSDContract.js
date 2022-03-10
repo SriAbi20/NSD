0dba330aae5f8bfbeb04a4a0bdef748c2932deb9
unknown <s.natarajan4482@student.leedsbeckett.ac.uk>
2022-03-11 05:25:29 +0530

    update one

--- src/components/NSDContract/NSDContract.js
+++ src/components/NSDContract/NSDContract.js
@@ -1,10 +1,75 @@
 import React from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+  
+  function createData(title, desc, learningoutcome, dateofcompletion ) {
+    return { title, desc, learningoutcome, dateofcompletion  };
+  }
+  
+  const rows = [
+    createData('To understand the main design principles of interaction design (30%)','I will attend lectures and carry out independent research through books and online resources to gain an understanding of how interaction design came about and why it is important in our everyday lives. From this the main principles of how interaction design works will focused on and then implemented whilst developing an interactive product.','The principles will explained through the online blog and be applied to the final product.','Date:07/05/2017'),
+    createData('To gain valuable skills in React.js (50%)','As a complete beginner with React.js coding language, I will watch youtube videos and complete tutorials and exercises to learn how to build basic applications using the appropriate coding such as React.js & Redux.','E-Portfolio also built using React.js, hence evidencing the skills acquired, the progress will be described on my blog.', 'Date:07/05/2022'),
+    createData('To adhere to Leeds Beckett branding guidelines (20%)','Leeds Beckett University has strict guidelines to follow when it comes to creating documents, marketing and media. I will discuss the product with the marketing team to ensure guidelines are met and follow the branding book requirements.', 'The final product will evidence that branding guidelines have been followed.','Date:07/05/2017')
+  ];
+
+
 
 const NSDContract = () =>{
     return (
-        <div>
-           NSDContract
-        </div>
+        <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="center">Title</StyledTableCell>
+              <StyledTableCell align="center">Description</StyledTableCell>
+              <StyledTableCell align="center">In what format will you evidence your learning outcome?</StyledTableCell>
+              <StyledTableCell align="center">Date of completion</StyledTableCell>
+              
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <StyledTableRow key={row.name}>
+                <StyledTableCell component="th" scope="row">
+                  {row.title}
+                </StyledTableCell>
+                <StyledTableCell align="right">{row.desc}</StyledTableCell>
+                <StyledTableCell align="right">{row.learningoutcome}</StyledTableCell>
+                <StyledTableCell align="right">{row.dateofcompletion}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     )
 }
 export default NSDContract;
--- src/components/Swot/Swot.js
+++ src/components/Swot/Swot.js
@@ -15,7 +15,7 @@ const Swot = () =>{
           Strength
         </Typography>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        ·     Quick Learner – New technology or digital software are subjects I feel quite confident with, I tend to learn new software packages easily and quickly and am not afraid to experiment to get the best results. 
+        ·     Quick Learner – New technology or digital software are subjects I feel quite confident with, I tend to learn new software packages easily and quickly and am not afraid to experiment to get the best results.
 ·   Communication – It will be important to engage with members of the team to recognise what is wanted from the project. My good verbal and written communication skills will help make the connection to ensure the project is successful.  
 ·  Commitment – This application is important to the helpdesk team that I work with, believing in the project and being committed to it is a great strength to see it through to the end. 
 ·    Motivation – I am highly motivated to improve the efficiency on the helpdesk, the project will provide support to colleagues and help to achieve this.
@@ -41,7 +41,7 @@ const Swot = () =>{
       Opportunities
         </Typography>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        ·   Use of online tutorials – The use of online blogs and tutorials as supplements to classroom learning will add broaden the knowledge base required to initiate the project.
+        ·   Use of online tutorials – The use of youtube and tutorials as supplements to classroom learning will add broaden the knowledge base required to initiate the project.
         </Typography>
         
         </CardContent>
@@ -52,7 +52,7 @@ const Swot = () =>{
       Threats
         </Typography>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        ·  Constant change in technology – As fast as we are learning about technology there are advancements in it, this could pose a threat that the application is no longer current when it is released.
+        ·  Constant update in technology – As fast as we are learning about technology there are advancements in it, this could pose a threat that the application is no longer current when it is released.
 ·   Bugs & Viruses – This is an external threat that is out of the control of the project, but it is something that could 
         </Typography>
         
